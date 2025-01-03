38d0e08d926527d4c84888c54ceeb574cd750287
Muthra <132976706+zorohack03@users.noreply.github.com>
2025-01-03 14:50:35 +0530

    Delete multiple.hs

--- multiple.hs
+++
@@ -1,7 +0,0 @@
-multiplyElements :: Num a => [a] -> a -> [a] 
-multiplyElements xs n = [x * n | x <- xs] 
-main :: IO () 
-main = do 
-        let result = multiplyElements [1, 2, 3, 4] 2 
-        print result   
-
