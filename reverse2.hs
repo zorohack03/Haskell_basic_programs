1827048ebe21b3ffd93c8b0cd53ee317724de821
Muthra <132976706+zorohack03@users.noreply.github.com>
2025-01-03 14:50:56 +0530

    Delete reverse2.hs

--- reverse2.hs
+++
@@ -1,8 +0,0 @@
-reverseList :: [a] -> [a] 
-reverseList [] = [] 
-reverseList (x:xs) = reverseList xs ++ [x] 
-main :: IO () 
-main = do 
-    let result = reverseList [1, 2, 3] 
-    print result  
-
